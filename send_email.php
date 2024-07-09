062f2fc3e252dee4c6d18bd7bb9796a52d6fb157
vinayaksutar-work <vinayaksutar.work@gmail.com>
2024-07-09 19:49:31 +0530

    added phpmailer library

--- send_email.php
+++ send_email.php
@@ -1,72 +1,40 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
 
+    $mail = new PHPMailer(true);
 
-    $to = "vinayaksutar.work@gmail.com";
-    $subject = "New Enquiry From " . $name;
-    
-   
-    $txt = "<html><head><style>
-h2{
-    background-color:#6f8291;
-    padding:10px;
-    color:white;
-}
-        table {
-            font-family: Arial, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+        $mail->SMTPAuth = true;
+        $mail->Username = 'vinayaksutar.work@gmail.com'; // SMTP username
+        $mail->Password = 'iognlbtyoiagsilz'; // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-        th, td {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-         
-        }
-        
+        // Recipients
+        $mail->setFrom($email);
+        $mail->addAddress('vinayaksutar.work@gmail.com'); // Add a recipient
 
-        th {
-            background-color: #f2f2f2;
-        }
-        a{
-            color:white;
-        }
-        </style></head><body>";
-    $txt .= "<h2>New Inquiry Received : <a>javalikarraomore.org</a></h2>";
-    $txt .= "<table border='1'>";
-    $txt .= " <tr>
-<td><b>Name</b></td>
-<td>".$name."</td>
-</tr>
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
 
-<tr>
-<td><b>Email </b></td>
-<td>".$email."</td>
-</tr>
-
-<tr>
-<td><b>Message </b></td>
-<td>".$message."</td>
-</tr>";
-
-    $txt .= "</table>";
-    $txt .= "</body></html>";
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= "From:" . $email;
-
-     mail($to, $subject, $txt, $headers);
-      echo json_encode(["status" => "success", "message" => "Our team will respond to you shortly"]);
-      
-
-    // Continue with sending the email...
-} else {
-    // If the request method is not POST, return an error
-    echo json_encode(["status" => "error", "message" => "Invalid request"]);
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 ?>
